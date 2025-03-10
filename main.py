be469daa534a243b82e5fda5d1315ec355fe1dad
Anton <anton.tsekh@gmail.com>
2025-03-10 21:50:21 +0200

    fixx

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 Main entry point for training the poker AI.
 """
 import os
+import sys
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 import argparse
 import torch
 import numpy as np
